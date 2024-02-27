fe1672a20d7ba9e2abf3c9b3729d9f58c00f42fc
Vlad Matei <vlad.mateei@gmail.com>
2024-02-27 22:28:03 +0000

    updating figure code

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    plt.figure(figsize=(12, 12))
+    plt.figure(figsize=(14, 16))
     sns.set()
+    sns.set_palette("husl")
     x_values = ["Scrambled Legs","Croissant Crawl","Morocco", "First Pollo Bar", "Comedy Club Date",
                 "St Christopher's", "Oxford Weekend", "BFT Day", "Pub Quiz", "Second Hyrox Session",
                 "Bingo Night", "4th of Feb in Victoria", "F45 with Sam", "Kate's Birthday",
@@ -22,11 +23,15 @@ if __name__ == '__main__':
     vladYValues = [0, 10, 20, 40, 50, 60, 70, 90, 95, 100, 99, 101]
     allVladYValues[:12] = vladYValues[:12]
     allVladYValues[12:] = [0 for value in allVladYValues[12:]]
-    plt.xticks(rotation=40)
-    plt.xlabel('t', fontsize=12)
-    plt.ylabel('f', fontsize=12)
+    plt.xticks(rotation=40, ha='right')
+    plt.xlabel('t', fontsize=14)
+    plt.ylabel('f', fontsize=14)
+    plt.tight_layout()
+    plt.grid(True)
     print(allVladYValues)
     plt.plot(x_values, allVladYValues)
+
+    plt.subplots_adjust(bottom=0.3)
     plt.show()
 
     print('hello')
